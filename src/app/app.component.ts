65bc565a5d10906dca2034545a4ed1a9ac980d02
a-kosanovic <kiselo.slatko@gmail.com>
2020-02-01 17:19:44 +0100

    feat(http): fetch and transform post data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +9,42 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
-  url = 'https://chronoplanner.firebaseio.com/';
+  loadedPosts: Post[] = [];
+  readonly POST_URL = 'https://chronoplanner.firebaseio.com/posts.json';
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    this.http.post(this.url + '/posts.json', JSON.stringify(postData)).subscribe( (resp: Response) => {
+    this.http.post<{name: string}>(this.POST_URL, JSON.stringify(postData)).subscribe( (resp) => {
       console.log('resp', resp);
     });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this._fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  _fetchPosts() {
+    this.http.get<{[keyId: string]: Post}>(this.POST_URL).pipe( map ((fbPayload) => {
+      const postsArray: Post[] = [];
+
+      for (const key in fbPayload) {
+        if (fbPayload.hasOwnProperty(key)) {
+          postsArray.push({...fbPayload[key], id: key });
+        }
+      }
+      return postsArray;
+
+    })).subscribe( (posts: Post[]) => {
+      this.loadedPosts = posts;
+    });
   }
 }
